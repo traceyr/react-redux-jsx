f66972c19d678a8daf6fcfac12945bbb510490c4
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2019-05-22 17:52:17 -0700

    added basic react app that does nothing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
+//Import react and react dom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Create a react component
+const App = () => {
+  const buttonText = { text: 'Click Me!' };
+  const labelText = 'Enter Name: ';
+  return (
+    <div>
+      <label htmlFor="name" className="label">{ labelText }</label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>{ buttonText.text }</button>
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Show react component on screen
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
